562a4d2934dca4235674343fed103b58396f55c0
ManeDM <mane657@hotmail.com.com>
2022-12-05 21:59:26 -0500

    Adicion de los datos de la tarjeta

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { forkJoin } from 'rxjs';
+import { PokeApiService } from 'src/app/service/poke-api.service';
 
 @Component({
   selector: 'app-details',
@@ -8,21 +10,29 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class DetailsComponent implements OnInit {
  
+  private urlPokemon: string = 'https://pokeapi.co/api/v2/pokemon'
+  private urlName: string = 'https://pokeapi.co/api/v2/pokemon-species';
+
+  public pokemon: any; 
+
   constructor (
-    private activateRoute: ActivatedRoute
+    private activateRoute: ActivatedRoute,
+    private pokeApiService: PokeApiService
   ) { }
 
   ngOnInit(): void {
-    this.pokemon;
+    this.getPokemon;
   }
 
-  get pokemon(){
+  get getPokemon(){
     const id = this.activateRoute.snapshot.params['id'];
+    const pokemon = this.pokeApiService.apiGetPokemons(`${this.urlPokemon}/${id}`);
+    const name = this.pokeApiService.apiGetPokemons(`${this.urlName}/${id}`);
 
-      return console.log(id);
-  }
-
-
-
-
-}
+    return forkJoin([pokemon, name]).subscribe(
+      res => {
+        this.pokemon = res;
+      }
+    )
+   }
+   }
