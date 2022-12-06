39f21bb202c6af7d16ef8e7b0eb985d8ca850a20
ManeDM <mane657@hotmail.com.com>
2022-12-05 21:15:16 -0500

    inicia el proceso de desarrollo de la tarjeta de detalles y su respectiva funcionalidad

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit {
+ 
+  constructor (
+    private activateRoute: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.pokemon;
+  }
+
+  get pokemon(){
+    const id = this.activateRoute.snapshot.params['id'];
+
+      return console.log(id);
+  }
+
+
+
 
 }
