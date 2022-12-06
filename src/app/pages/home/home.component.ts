7a8230f37ea22a914b7ffe52cf70ac494cf1cf7a
ManeDM <mane657@hotmail.com.com>
2022-12-06 16:51:47 -0500

    primeros ajustes a la creación del poketeam

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PokeApiService } from 'src/app/service/poke-api.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  
+  private setAllPokemons: any; 
+  public getAllPokemons: any;
 
+  
+
+  constructor(
+    private pokeApiService: PokeApiService
+  ) { }
+
+  ngOnInit(): void {
+    this.pokeApiService.apiListAllPokemons.subscribe(
+      res => {
+        this.setAllPokemons = res.results;
+        this.getAllPokemons = this.setAllPokemons;
+         
+      },
+     
+    );
+  }
 }
