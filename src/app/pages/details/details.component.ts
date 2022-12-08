aa8fe33f34fc206767230a60b5fc410639c1b584
ManeDM <mane657@hotmail.com.com>
2022-12-08 14:05:00 -0500

    correcion de error en la correcion de estilos

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { forkJoin } from 'rxjs';
+
+//Services
 import { PokeApiService } from 'src/app/service/poke-api.service';
 
 @Component({
@@ -9,31 +11,36 @@ import { PokeApiService } from 'src/app/service/poke-api.service';
   styleUrls: ['./details.component.scss']
 })
 export class DetailsComponent implements OnInit {
- 
-  private urlPokemon: string = 'https://pokeapi.co/api/v2/pokemon'
+
+  private urlPokemon: string = 'https://pokeapi.co/api/v2/pokemon';
   private urlName: string = 'https://pokeapi.co/api/v2/pokemon-species';
 
-  public pokemon: any; 
- 
+  public pokemon: any;
+  public isLoading: boolean = false;
+  public apiError: boolean = false;
 
-  constructor (
-    private activateRoute: ActivatedRoute,
+  constructor(
+    private activatedRoute: ActivatedRoute,
     private pokeApiService: PokeApiService
   ) { }
 
   ngOnInit(): void {
-    this.getPokemon;
+    this.getPokemon();
   }
 
-  get getPokemon(){
-    const id = this.activateRoute.snapshot.params['id'];
+  public getPokemon(){
+    const id = this.activatedRoute.snapshot.params['id'];
     const pokemon = this.pokeApiService.apiGetPokemons(`${this.urlPokemon}/${id}`);
     const name = this.pokeApiService.apiGetPokemons(`${this.urlName}/${id}`);
 
     return forkJoin([pokemon, name]).subscribe(
       res => {
         this.pokemon = res;
-              }
-    )
-   }
-   }
+        this.isLoading = true;
+      },
+      error => {
+        this.apiError = true;
+      }
+    );
+  }
+}
